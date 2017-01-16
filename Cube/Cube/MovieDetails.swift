6c7a5ddafd198a78cac5d238291f006bfb9024b5
iamsimranjot <iamsimranjot@gmail.com>
2017-01-16 22:18:49 +0530

    Folders Restructuring & Added Movie Details Model.

---
+++ Cube/Cube/MovieDetails.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDetails.swift
+//  Cube
+//
+//  Created by SimranJot Singh on 16/01/17.
+//  Copyright © 2017 SimranJot Singh. All rights reserved.
+//
+
+struct MovieDetails {
+    
+    //MARK: Properties 
+    
+    let title: String
+    let genre: String
+    let releaseDate: String
+    let plot: String
+    let ratings: String
+    let posterUrl: String
+    
+    //MARK: Initializer
+    
+    init(title: String, genre: String, releaseDate: String, plot: String, ratings: String, imageUrl: String) {
+        self.title = title
+        self.genre = genre
+        self.releaseDate = releaseDate
+        self.plot = plot
+        self.ratings = ratings
+        self.posterUrl = imageUrl
+    }
+    
+}
+
