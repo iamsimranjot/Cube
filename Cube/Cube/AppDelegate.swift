b7deca755ea705c366060bcebd17011a5cd57781
iamsimranjot <iamsimranjot@gmail.com>
2017-01-18 04:54:50 +0530

    Details Added.

--- Cube/Cube/AppDelegate.swift
+++ Cube/Cube/AppDelegate.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2017 SimranJot Singh. All rights reserved.
 //
 
+/* The Project meets the specifications of searching a movie and then displaying its required details to the user. Allowing user to filter with type option has been modified as the results shown on seacrh are already filterd by type for better user experience. Cool Animations has been applied to both the view controllers also. The Application has followed the MVC Design Pattern. Proper commenting of code and eficient use of language has been done in regards with the coding standard. */
+
 import UIKit
 
 @UIApplicationMain
